a2383c6ae11814522eb10f73c39eae7b83811122
Deepeshnama <deepeshnama0786@gmail.com>
2025-02-21 19:06:50 +0530

    added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URI);
+   await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongoose connected");
   } catch (error) {
     console.log(error.message);
